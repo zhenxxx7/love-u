f5f814f84bbb83e0a2390c52f3539e3bd5b8d990
zhenxxx7 <jordy.zeventh@gmail.com>
2022-12-23 21:27:23 +0700

    update content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Modal from './component/modal';
 import './App.css';
 
 function App() {
@@ -8,8 +9,20 @@ function App() {
     document.getElementById('no').style
       .transform = `translate(${x}px, ${y}px)`;
   }
+
+  const accept = () => {
+    setShowModal(true);
+  }
+
+  const [showModal, setShowModal] = React.useState(false);
+  const handleOnClose = () => setShowModal(false);
+
+  if (showModal) {
+    document.body.style.overflow = 'hidden';
+  }
+  
   return (
-    <div className="h-screen flex flex-col items-center justify-center font-inte">
+    <div className="h-screen flex flex-col items-center justify-center font-inte bg-[#FFD4B2]">
       <div className="items-center-center">
         <h1 className="text-4xl font-bold text-primary">
           Hello pretty girl
@@ -19,9 +32,9 @@ function App() {
         </h2>
       </div>
       <div className="flex flex-col items-center text-amber-600 text-lg py-10">
-        <p className="flex flex-col items-center">
+        <p className="flex flex-col items-center text-center">
           but I don't know how to tell you that I love you so much and I want to be with you forever
-          <span className="flex flex-col items-center">
+          <span className="flex flex-col items-center font-bold text-[#C58940]">
             Do you wanna be my girlfriend?
           </span>
         </p>
@@ -29,8 +42,9 @@ function App() {
 
         <div className='flex gap-2'>
           <button id='yes' className="bg-color1 rounded-full px-10 py-2 mt-4 "
-            >
-            <span className="text-2xl font-bold text-white">
+          onClick={accept}
+          >
+            <span className="text-2xl font-bold text-[#CEEDC7]">
               Yes
             </span>
           </button>
@@ -38,11 +52,14 @@ function App() {
           <button id='no' className="bg-color1 rounded-full px-12 py-2 mt-4"
             onClick={reject}
             >
-            <span className="text-2xl font-bold text-white">
+            <span className="text-2xl font-bold text-[#CEEDC7]">
               No
             </span>
           </button>
         </div>
+
+        <Modal onClose={handleOnClose} visible={showModal} />
+
     </div>
   );
 }
---
+++ src/component/modal.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import love from "../component/img/love-peach.gif";
+
+export default function Modal({visible, onClose}) {
+
+    if (!visible) return null;
+  return (
+    <div className="fixed backdrop-blur z-10  inset-0 overflow-y-auto max-sm:overflow-hidden max-sm:-mt-[14vh]">
+        <div class=" block items-end justify-center min-h-screen pt-[35vh] px-4 pb-[65vh] text-center sm:block sm:p-0">
+        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                    <div class="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                        <div class="flex flex-col items-center">
+                            <h1>
+                                <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                    <h3 class="text-lg leading-6 font-medium text-secondary" id="modal-headline">
+                                        thank you because you have been to be my girlfriend
+                                    </h3>
+                                    <img src={love} alt="" />
+                                </div>
+                                <button
+                                    type="button" className="bg-color1 text-lg font-bold text-[#CEEDC7] rounded-full w-[20%] h-8"
+                                    onClick={onClose}
+                                >
+                                    OK
+                                </button>
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+  );
+}
