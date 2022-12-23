192a2e84824cdd852bbcb7dc9b0e93e8a8ea4b47
zhenxxx7 <jordy.zeventh@gmail.com>
2022-12-23 19:14:20 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'primary': '#434343',
+        'secondary': '#FFB546',
+        'color1' : '#FDAE8F',
+      },
+      
+      fontFamily: {
+        inte: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  container: {
+    center: true,
+  },
+  plugins: [],
+}
